5fd6ab78aa3b232876d44f64849acc6716925f8c
edsonfervo16mx <edson55_@hotmail.com>
2020-09-23 22:38:06 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def mensaje(nombre,paterno,materno):
+    print('Hola, mi nombre es: {} y mis apellidos son {} {}'.format(nombre,paterno,materno))
+
+def suma(a,b):
+    print(f'El resultado es: {a+b}')
+
+def resta(a,b):
+    print(f'El resultado es: {a - b}')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm edsonfer')
+    suma(5,8)
+    resta(10,8)
+    mensaje('Edson Fernando', 'Ventura', 'Oropeza')
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
