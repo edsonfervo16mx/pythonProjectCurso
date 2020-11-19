d52775c8151cc0f048a1e72bb59a8c003601eaa3
edsonfervo16mx <edson55_@hotmail.com>
2020-11-19 08:03:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 # This is a sample Python script.
 
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print('Hi', {name})  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def mensaje(nombre,paterno,materno):
     print('Hola, mi nombre es: {} y mis apellidos son {} {}'.format(nombre,paterno,materno))
 
 def suma(a,b):
-    print('El resultado es:', {a+b})
+    print(f'El resultado es: {a+b}')
 
 def resta(a,b):
-    print('El resultado es:', {a - b})
+    print(f'El resultado es: {a - b}')
 
     '''
     Comentario de varias lineas en
@@ -26,6 +30,9 @@ def tipos():
     print(float(b))
     print(str(t))
     print(bool(v))
+
+
+    
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
